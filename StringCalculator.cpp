a4a5092c64eb624051f83414ff989e2d8a6ecbf3
vishnucv1992 <cv.vishnu@in.bosch.com>
2024-06-27 15:38:02 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,5 +2,14 @@
 
 int StringCalculator::add(string input)
 {
-        return 0;
+    std::stringstream ss(input);
+    std::string token;
+    int sum = 0;
+    
+    // Split by comma and calculate sum
+    while (getline(ss, token, ',')) {
+        sum += std::stoi(token); // Convert string to integer and sum
+    }
+    
+    return sum;
 }
